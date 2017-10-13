0ea8a9e3e7c8c609da158b19be44d2903d569ba4
alswnsWkd21 <djflqjfl0209@gmail.com>
2017-10-13 16:39:25 +0900

    add ajax button and json

--- 05/ajax.js
+++ 05/ajax.js
@@ -1,1 +1,11 @@
-console.log('ajax.js')
+console.log('ajax.js');
+var btn = document.getElementById('btn');
+btn.addEventListener('click', load);
+
+function load(){
+  console.log('load');
+  var request = new XMLHttpRequest();
+
+  request.open('GET', 'dummy', true);
+  console.log(reuqest);
+}
