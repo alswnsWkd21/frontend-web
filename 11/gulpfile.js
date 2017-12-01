f7f01a23d4a6032e56bdd42f06b6b3a6bc8c6ac9
alswnsWkd21 <djflqjfl0209@gmail.com>
2017-12-01 16:07:23 +0900

    13주차 까먹고 누락

---
+++ 11/gulpfile.js
@@ -0,0 +1,14 @@
+//gulpfile.js
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+  console.log('default1')
+  console.log('default1')
+  console.log('default1')
+});
+gulp.task('default1', function() {
+  // place code for your default task here
+  console.log('default1')
+});
